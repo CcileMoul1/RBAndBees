4511b257115ee1b71f06758d8c6dc816149c6da7
Ccile <moulin.ccile@yahoo.fr>
2026-07-31 19:17:25 +0200

    est-ce bon cette fois ?

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,8 +2,8 @@
 
 test('returns a successful response', function () {
     $response = $this->get('/');
-    
-    echo "STATUS=" . $response->status() . PHP_EOL;
+
+    echo 'STATUS='.$response->status().PHP_EOL;
 
     $response->assertOk();
 });
